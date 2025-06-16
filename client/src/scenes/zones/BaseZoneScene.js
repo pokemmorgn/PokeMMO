fa1bc78ca3a61e042d2cdced8f95a3f817eb37f3
pokemmorgn <pokemmorgn@gmail.com>
2025-06-16 12:29:35 +0200

    Update BaseZoneScene.js

--- client/src/scenes/zones/BaseZoneScene.js
+++ client/src/scenes/zones/BaseZoneScene.js
@@ -261,7 +261,7 @@ export class BaseZoneScene extends Phaser.Scene {
 
 const fetchLastPosition = async (identifier) => {
   try {
-const res = await fetch(`http://localhost:2567/api/playerData?username=${encodeURIComponent(username)}`);
+const res = await fetch(`http://vps-8ca3aa34.vps.ovh.net:2567/api/playerData?username=${encodeURIComponent(username)}`);
     if (res.ok) {
       const data = await res.json();
       console.log("DEBUG API response data:", data); // <---- AJOUTE ÇA
