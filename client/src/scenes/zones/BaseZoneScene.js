1fb2199d910055063f6ea9cbbc80278960fed595
pokemmorgn <pokemmorgn@gmail.com>
2025-06-16 22:43:50 +0200

    Update BaseZoneScene.js

--- client/src/scenes/zones/BaseZoneScene.js
+++ client/src/scenes/zones/BaseZoneScene.js
@@ -23,8 +23,8 @@ export class BaseZoneScene extends Phaser.Scene {
 
     if (!this.textures.exists('BoyWalk')) {
   this.load.spritesheet('BoyWalk', 'assets/character/BoyWalk.png', {
-    frameWidth: 32,
-    frameHeight: 48,
+    frameWidth: 16,
+    frameHeight: 16,
   });
 }
   }
