e14b0bfab6a6cd16a492fc8849feabea52547ff8
pokemmorgn <pokemmorgn@gmail.com>
2025-06-17 23:09:36 +0200

    Update BaseZoneScene.js

--- client/src/scenes/zones/BaseZoneScene.js
+++ client/src/scenes/zones/BaseZoneScene.js
@@ -505,14 +505,14 @@ this.applyVisualSettings();
 applyVisualSettings() {
   switch (this.mapKey) {
     case 'ProfLaboInt':
-      if (this.cameraManager) this.cameraManager.setZoom(-2);
+      if (this.cameraManager) this.cameraManager.setZoom(0.5);
       const playerLab = this.playerManager?.getMyPlayer();
       if (playerLab) playerLab.setScale(1);
       break;
 
     case 'Village':
     case 'VillageRoom':
-      if (this.cameraManager) this.cameraManager.setZoom(4);
+      if (this.cameraManager) this.cameraManager.setZoom(0.5);
       const playerVillage = this.playerManager?.getMyPlayer();
       if (playerVillage) playerVillage.setScale(1);
       break;
