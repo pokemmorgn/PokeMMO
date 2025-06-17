7c2fc524c21054b5d200fdbe752afbb45b0ba5da
pokemmorgn <pokemmorgn@gmail.com>
2025-06-17 15:18:14 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,3 @@
 import { listen } from "@colyseus/tools";
 import appConfig from "./app.config";
-import https from "https";
-import fs from "fs";
-
-const httpsOptions = {
-    cert: fs.readFileSync('/etc/letsencrypt/live/pokerune.cloud/fullchain.pem'),
-    key: fs.readFileSync('/etc/letsencrypt/live/pokerune.cloud/privkey.pem')
-};
-const httpsServer = https.createServer(httpsOptions);
-
-listen(appConfig, 2567, httpsServer).then(() => {
-    console.log("✅ Colyseus écoute sur https://pokerune.cloud:2567");
-});
+listen(appConfig, 2567);
