32cfc3b598d9393a671e92ff6b9044169948303d
pokemmorgn <pokemmorgn@gmail.com>
2025-06-17 12:31:50 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,15 @@
-/**
- * IMPORTANT:
- * ---------
- * Do not manually edit this file if you'd like to host your server on Colyseus Cloud
- *
- * If you're self-hosting (without Colyseus Cloud), you can manually
- * instantiate a Colyseus Server as documented here:
- *
- * See: https://docs.colyseus.io/server/api/#constructor-options
- */
+import fs from "fs";
+import https from "https";
 import { listen } from "@colyseus/tools";
-
-// Import Colyseus config
 import app from "./app.config";
 
-// Create and listen on 2567 (or PORT environment variable.)
-listen(app);
+const sslOptions = {
+  key: fs.readFileSync("/home/ubuntu/pokerune_certs/privkey.pem"),
+  cert: fs.readFileSync("/home/ubuntu/pokerune_certs/fullchain.pem"),
+};
+
+const server = https.createServer(sslOptions);
+
+listen(app, { server }).then(() => {
+  console.log("✅ Serveur HTTPS lancé sur https://pokerune.cloud");
+});
