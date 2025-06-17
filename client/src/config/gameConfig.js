034796136eca289c7931704f70811ae0371ab7d7
pokemmorgn <pokemmorgn@gmail.com>
2025-06-17 22:01:56 +0200

    Update gameConfig.js

--- client/src/config/gameConfig.js
+++ client/src/config/gameConfig.js
@@ -11,7 +11,7 @@ export const GAME_CONFIG = {
   antialias: false,
   
 server: {
-  url: `${location.protocol === "https:" ? "wss" : "ws"}://${location.host}/api`,
+  url: `${location.protocol === "https:" ? "wss" : "ws"}://${location.host}/ws`,
   roomName: ""
 },
   
