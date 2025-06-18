5c44342c28ccb8a071fd98f1673220065e6d7caa
pokemmorgn <pokemmorgn@gmail.com>
2025-06-19 00:19:10 +0200

    Update VillageHouse1Room.ts

--- server/src/rooms/VillageHouse1Room.ts
+++ server/src/rooms/VillageHouse1Room.ts
@@ -9,14 +9,14 @@ export class VillageHouse1Room extends BaseRoom {
   protected defaultX = 300;
   protected defaultY = 200;
 
-  protected calculateSpawnPosition(targetZone: string): { x: number, y: number } {
-    switch (targetZone) {
-      case 'VillageScene':      return { x: 500, y: 400 };
-      case 'VillageLabScene':   return { x: 70,  y: 240 };
-      // Ajoute d’autres zones si besoin
-      default:                  return { x: this.defaultX, y: this.defaultY };
-    }
+ public calculateSpawnPosition(targetZone: string): { x: number, y: number } {
+  switch (targetZone) {
+    case "VillageScene":
+      return { x: 181, y: 281 }; // Position où spawn le joueur s'il vient de BeachScene
+    default:
+      return { x: this.defaultX, y: this.defaultY };
   }
+}
 
   onCreate(options: any): void {
     super.onCreate(options);
