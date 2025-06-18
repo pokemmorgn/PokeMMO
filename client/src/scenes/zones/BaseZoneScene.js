599dcfd33ce1b4f7ef7bd054a8336c1e0c9c11ac
pokemmorgn <pokemmorgn@gmail.com>
2025-06-18 17:15:13 +0200

    Update BaseZoneScene.js

--- client/src/scenes/zones/BaseZoneScene.js
+++ client/src/scenes/zones/BaseZoneScene.js
@@ -59,7 +59,7 @@ export class BaseZoneScene extends Phaser.Scene {
 
   getExistingNetwork() {
     // Liste des scènes qui pourraient avoir le NetworkManager
-    const scenesToCheck = ['BeachScene', 'VillageScene', 'Road1Scene', 'VillageLabScene', 'VillageHouse1Scene'];
+    const scenesToCheck = ['BeachScene', 'VillageScene', 'Road1Scene', 'VillageLabScene', 'VillageHouse1Scene, Lavandia'];
     for (const sceneName of scenesToCheck) {
       const scene = this.scene.manager.getScene(sceneName);
       if (scene && scene.networkManager) {
@@ -349,6 +349,9 @@ onPlayerPositioned(player, initData) {
           break;
           case 'house1':
           roomName = 'VillageHouse1Room';
+          break;
+          case 'lavandua':
+          roomName = 'LavandiaRoom';
           break;
         default:
           roomName = 'BeachRoom';
