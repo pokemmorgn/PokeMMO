25f2035259fa6b53345eaa95a79b466845d7fadb
pokemmorgn <pokemmorgn@gmail.com>
2025-06-19 00:23:55 +0200

    Update LavandiaRoom.ts

--- server/src/rooms/LavandiaRoom.ts
+++ server/src/rooms/LavandiaRoom.ts
@@ -1,24 +1,22 @@
-// ===============================================
 // LavandiaRoom.ts - Room Lavandia, héritant de BaseRoom
-// ===============================================
 import { BaseRoom } from "./BaseRoom";
+import type { SpawnData } from "./BaseRoom"; // Import du type SpawnData
 
 export class LavandiaRoom extends BaseRoom {
   protected mapName = "LavandiaRoom";
   protected defaultX = 350;
   protected defaultY = 750;
 
- public calculateSpawnPosition(spawnData: SpawnData): { x: number, y: number } {
-  const targetZone = spawnData.targetZone;
+  public calculateSpawnPosition(spawnData: SpawnData): { x: number, y: number } {
+    const targetZone = spawnData.targetZone;
 
-  switch (targetZone) {
-    case "Road1Scene":
-      return { x: 82, y: 911 }; // Position où spawn le joueur s'il vient de BeachScene
-    default:
-      return { x: this.defaultX, y: this.defaultY };
+    switch (targetZone) {
+      case "Road1Scene":
+        return { x: 82, y: 911 }; // Position où spawn le joueur s'il vient de Road1Scene
+      default:
+        return { x: this.defaultX, y: this.defaultY };
+    }
   }
-}
-
 
   onCreate(options: any) {
     super.onCreate(options);
