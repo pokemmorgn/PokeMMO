0744924815000c88069e3d2a620f751ef3758b21
pokemmorgn <pokemmorgn@gmail.com>
2025-06-19 00:18:35 +0200

    Update LavandiaRoom.ts

--- server/src/rooms/LavandiaRoom.ts
+++ server/src/rooms/LavandiaRoom.ts
@@ -8,15 +8,15 @@ export class LavandiaRoom extends BaseRoom {
   protected defaultX = 350;
   protected defaultY = 750;
 
-  protected calculateSpawnPosition(targetZone: string): { x: number, y: number } {
-    switch (targetZone) {
-      case 'Route8Scene':         return { x: 40,  y: 350 };
-      case 'Route10Scene':        return { x: 350, y: 60  };
-      case 'PoketowerScene':      return { x: 550, y: 150 };
-      case 'LavandiaHouse1Scene': return { x: 150, y: 670 };
-      default:                    return { x: this.defaultX, y: this.defaultY };
-    }
+ public calculateSpawnPosition(targetZone: string): { x: number, y: number } {
+  switch (targetZone) {
+    case "BeachScene":
+      return { x: 82, y: 911 }; // Position où spawn le joueur s'il vient de BeachScene
+    default:
+      return { x: this.defaultX, y: this.defaultY };
   }
+}
+
 
   onCreate(options: any) {
     super.onCreate(options);
