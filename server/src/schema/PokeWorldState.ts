8f242d9b6c9785beb9fa958762b374e33a21f311
pokemmorgn <pokemmorgn@gmail.com>
2025-06-18 18:28:40 +0200

    Update PokeWorldState.ts

--- server/src/schema/PokeWorldState.ts
+++ server/src/schema/PokeWorldState.ts
@@ -4,7 +4,7 @@ export class Player extends Schema {
   @type("number") x: number = 300;
   @type("number") y: number = 300;
   @type("string") map: string = "";
-  @type("string") name: string;
+  @type("string") map: string = "";
   // 👇 AJOUTE pour les anims
   @type("string") direction: string = "down";
   @type("boolean") isMoving: boolean = false;
