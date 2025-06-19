ba4f4f5ba7f4fc556ab744d8c52cb5a87f31418a
pokemmorgn <pokemmorgn@gmail.com>
2025-06-19 10:20:29 +0200

    Update convertOwnedPokemonToTeam.ts

--- server/src/utils/convertOwnedPokemonToTeam.ts
+++ server/src/utils/convertOwnedPokemonToTeam.ts
@@ -1,13 +1,14 @@
-import { TeamPokemon } from "../schema/PokeWorldState"; // adapte le chemin
+import { TeamPokemon } from "../schema/PokeWorldState";
+import { IOwnedPokemon } from "../models/OwnedPokemon"; // ou ton interface
 
-export function convertOwnedPokemonToTeam(poke): TeamPokemon {
+export function convertOwnedPokemonToTeam(poke: IOwnedPokemon): TeamPokemon {
   const teamPoke = new TeamPokemon();
   teamPoke.id = poke._id.toString();
   teamPoke.pokemonId = poke.pokemonId;
   teamPoke.level = poke.level;
   teamPoke.nickname = poke.nickname;
   teamPoke.shiny = poke.shiny;
-  teamPoke.gender = poke.gender; // Ajoute tout ce que tu veux exposer
-  // teamPoke.ivs = poke.ivs; // etc.
+  teamPoke.gender = poke.gender;
+  // ...autres champs
   return teamPoke;
 }
