fc81babee20b5f3457879c88ba6a61c9532360bc
pokemmorgn <pokemmorgn@gmail.com>
2025-06-19 09:37:36 +0200

    Update PlayerData.ts

--- server/src/models/PlayerData.ts
+++ server/src/models/PlayerData.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const PlayerDataSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   gold: { type: Number, default: 0 },
+  pokemons: [{ type: String }],        // (optionnel - legacy, à migrer)
   team: [{ type: mongoose.Schema.Types.ObjectId, ref: "OwnedPokemon" }], // <-- Ajouté ici !
   lastX: { type: Number, default: 300 },
   lastY: { type: Number, default: 300 },
