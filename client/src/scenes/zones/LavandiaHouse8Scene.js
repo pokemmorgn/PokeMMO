24fc8ad272336773989604407d1177a2f16176a8
pokemmorgn <pokemmorgn@gmail.com>
2025-06-22 23:39:11 +0200

    Update LavandiaHouse8Scene.js

--- client/src/scenes/zones/LavandiaHouse8Scene.js
+++ client/src/scenes/zones/LavandiaHouse8Scene.js
@@ -1,4 +1,18 @@
 import { BaseZoneScene } from './BaseZoneScene.js';
 
 export class LavandiaHouse8Scene extends BaseZoneScene {
-  constructor
+  constructor() {
+    super('LavandiaHouse8Scene', 'lavandiahouse9');
+    this.transitionCooldowns = {};
+  }
+
+  onPlayerReady(myPlayer) {
+    super.onPlayerReady(myPlayer);
+    console.log(`[LavandiaHouse9Scene] Mon joueur est prêt à (${myPlayer.x}, ${myPlayer.y})`);
+  }
+
+  cleanup() {
+    this.transitionCooldowns = {};
+    super.cleanup();
+  }
+}
