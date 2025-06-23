44b7deb92264195d9d46b7a85f45737340a938a0
pokemmorgn <pokemmorgn@gmail.com>
2025-06-23 07:49:56 +0200

    Update BaseZoneScene.js

--- client/src/scenes/zones/BaseZoneScene.js
+++ client/src/scenes/zones/BaseZoneScene.js
@@ -257,7 +257,7 @@ export class BaseZoneScene extends Phaser.Scene {
         return;
       }
       
-      console.log(`📊 [${this.scene.key}] State reçu:`, {
+      //console.log(`📊 [${this.scene.key}] State reçu:`, {
         playersCount: state.players?.size || 0,
         isFiltered: !!state.players,
         type: state.players instanceof Map ? 'Map' : 'Object'
