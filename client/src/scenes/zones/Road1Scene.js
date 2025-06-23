56d6a94049b03609f3e3d518e0ab95e7fcf86c8c
pokemmorgn <pokemmorgn@gmail.com>
2025-06-23 11:26:33 +0200

    Update Road1Scene.js

--- client/src/scenes/zones/Road1Scene.js
+++ client/src/scenes/zones/Road1Scene.js
@@ -28,8 +28,6 @@ export class Road1Scene extends BaseZoneScene {
     console.log("🚨 DEBUT Road1Scene.create()");
     super.create();
     console.log("✅ BaseZoneScene.create() appelé");
-  this.playerManager = new PlayerManager(this);
-  window.globalNetworkManager.playerManager = this.playerManager;
     this.add.text(16, 16, 'Route 1 - Route vers l\'aventure\nFlèches pour se déplacer\nAppuyez sur "D" pour les hitboxes', {
       font: '18px monospace',
       fill: '#ffffff',
