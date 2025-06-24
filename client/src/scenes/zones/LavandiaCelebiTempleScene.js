884586d8c61fe607f3c4ebef24bde481d49d72ca
pokemmorgn <pokemmorgn@gmail.com>
2025-06-24 16:41:51 +0200

    Update and rename LavandiaCelibiTempleScene.js to LavandiaCelebiTempleScene.js

--- client/src/scenes/zones/LavandiaCelebiTempleScene.js
+++ client/src/scenes/zones/LavandiaCelebiTempleScene.js
@@ -1,14 +1,14 @@
 import { BaseZoneScene } from './BaseZoneScene.js';
 
-export class LavandiaCelibiTempleScene extends BaseZoneScene {
+export class LavandiaCelebiTempleScene extends BaseZoneScene {
   constructor() {
-    super('LavandiaCelibiTempleScene', 'lavandiacelibitemple');
+    super('LavandiaCelebiTempleScene', 'lavandiacelibitemple');
     this.transitionCooldowns = {};
   }
 
   onPlayerReady(myPlayer) {
     super.onPlayerReady(myPlayer);
-    console.log(`[LavandiaCelibiTempleScene] Mon joueur est prêt à (${myPlayer.x}, ${myPlayer.y})`);
+    console.log(`[LavandiaCelebiTempleScene] Mon joueur est prêt à (${myPlayer.x}, ${myPlayer.y})`);
   }
 
   cleanup() {
