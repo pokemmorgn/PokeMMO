de958a8be317f6fd19330fdd77d1a7d32bc77a68
pokemmorgn <pokemmorgn@gmail.com>
2025-06-25 14:13:43 +0200

    Update PlayerManager.js

--- client/src/game/PlayerManager.js
+++ client/src/game/PlayerManager.js
@@ -189,8 +189,8 @@ player.sessionId = sessionId;
 player.body.setCollideWorldBounds(true); // bloque le joueur dans les bords map
 
 // Garde le setSize et offset si ça colle bien à ton sprite
-player.body.setSize(64, 64);
-player.body.setOffset(48, 64);
+player.body.setSize(16, 16);
+player.body.setOffset(8, 16);
 
 if (this.scene.anims.exists('idle_down')) player.anims.play('idle_down');
     player.lastDirection = 'down';
