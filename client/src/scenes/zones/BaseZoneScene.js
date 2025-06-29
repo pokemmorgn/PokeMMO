a01f71d88542996d7a6e349502913b31daf969bc
pokemmorgn <pokemmorgn@gmail.com>
2025-06-29 16:57:27 +0200

    Update BaseZoneScene.js

--- client/src/scenes/zones/BaseZoneScene.js
+++ client/src/scenes/zones/BaseZoneScene.js
@@ -5,7 +5,7 @@
 
 import { PlayerManager } from "../../game/PlayerManager.js";
 import { CameraManager } from "../../camera/CameraManager.js";
-import { NpcManager } from "../../game/NpcManager.js";
+import { NpcManager } from "../../game/NpcManager.ts";
 import { QuestSystem } from "../../game/QuestSystem.js";
 import { InventorySystem } from "../../game/InventorySystem.js";
 import { InteractionManager } from "../../game/InteractionManager.js";
