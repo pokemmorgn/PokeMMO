7146087e613e954e6ed751a01f5020593c991081
pokemmorgn <pokemmorgn@gmail.com>
2025-06-30 10:25:29 +0200

    Update NotificationManager.js

--- client/src/components/NotificationManager.js
+++ client/src/components/NotificationManager.js
@@ -475,6 +475,7 @@ export class NotificationManager {
   // Notification pour les quêtes
   questNotification(questName, action = 'started', options = {}) {
     const actionText = {
+questGranted: 'Quest accepted',
 started:   'Quest accepted',
 completed: 'Quest completed',
 failed:    'Quest failed',
