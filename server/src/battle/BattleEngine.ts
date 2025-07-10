5fe3b33a411db0b0a8b15820beca984aee5f1a04
pokemmorgn <pokemmorgn@gmail.com>
2025-07-10 12:45:49 +0200

    Update BattleEngine.ts

--- server/src/battle/BattleEngine.ts
+++ server/src/battle/BattleEngine.ts
@@ -12,7 +12,7 @@ import { BroadcastManager } from './modules/BroadcastManager';
 import { BroadcastManagerFactory } from './modules/broadcast/BroadcastManagerFactory';
 import { SpectatorManager } from './modules/broadcast/SpectatorManager';
 import { BATTLE_TIMINGS } from './modules/BroadcastManager';
-import { BattleConfig, BattleGameState, BattleResult, BattleAction, BattleModule, PlayerRole } from './types/BattleTypes';
+import { BattleConfig, BattleGameState, BattleResult, BattleAction, BattleModule, PlayerRole, Pokemon } from './types/BattleTypes';
 
 /**
  * BATTLE ENGINE - POKÉMON ROUGE/BLEU ABSOLUMENT AUTHENTIQUE
