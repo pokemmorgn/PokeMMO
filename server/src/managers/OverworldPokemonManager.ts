cc25ca165105bece08e78e091a586a270e71771d
pokemmorgn <pokemmorgn@gmail.com>
2025-07-10 10:34:05 +0200

    Update OverworldPokemonManager.ts

--- server/src/managers/OverworldPokemonManager.ts
+++ server/src/managers/OverworldPokemonManager.ts
@@ -359,10 +359,7 @@ export class OverworldPokemonManager {
   }
 }
 
-  onMessage(client: any, type: string, message: any) {
-  if (type === 'overworldPokemonSpawnResponse') {
-    this.overworldPokemonManager.handleClientSpawnResponse(client, message);
-  }
+
   }
   private updateAllPokemon(deltaTime: number): void {
     this.overworldPokemon.forEach((pokemon, id) => {
