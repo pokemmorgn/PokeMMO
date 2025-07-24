000664ca1d8b74644f157389945e9ea119a49393
pokemmorgn <pokemmorgn@gmail.com>
2025-07-24 21:09:13 +0200

    Update index.js

--- client/src/Quest/index.js
+++ client/src/Quest/index.js
@@ -6,16 +6,14 @@ export { QuestIcon } from './QuestIcon.js';
 export { QuestUI } from './QuestUI.js';
 
 // === ALIAS POUR COMPATIBILITÉ ===
-
 export { createQuestSystem as createQuestModule } from './QuestSystem.js';
 export { QuestSystem as QuestModule } from './QuestSystem.js';
 
 // === FONCTION PRINCIPALE ===
-
-export async function initializeQuestSystem(gameRoom, networkManager, scene) {
+export async function initializeQuestSystem(gameRoom, networkManager) {
   const { createQuestSystem } = await import('./QuestSystem.js');
-  return createQuestSystem(gameRoom, networkManager, scene);
+  return createQuestSystem(gameRoom, networkManager);
 }
 
-console.log('📖 [Quest/index] Système unifié chargé');
+console.log('📖 [Quest/index] Système unifié simplifié chargé');
 console.log('🎯 Utilisez createQuestSystem(gameRoom, networkManager) pour initialiser');
