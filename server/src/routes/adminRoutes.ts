ae1b7206af962fff21f5f0af39436d09da668ef6
pokemmorgn <pokemmorgn@gmail.com>
2025-08-08 16:16:33 +0200

    Update adminRoutes.ts

--- server/src/routes/adminRoutes.ts
+++ server/src/routes/adminRoutes.ts
@@ -2434,7 +2434,7 @@ router.get('/zones/:zoneId/npcs', requireMacAndDev, async (req: any, res) => {
         interactionRadius: npc.interactionRadius || 32,
         canWalkAway: npc.canWalkAway !== false,
         autoFacePlayer: npc.autoFacePlayer !== false,
-repeatable: npcData.repeatable !== false,
+repeatable: npcJson.repeatable !== false,
         cooldownSeconds: npc.cooldownSeconds || 0,
         
         // Système de quêtes
